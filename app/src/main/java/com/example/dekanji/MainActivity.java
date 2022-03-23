ab2b70a4c542a7ceedf01adab154b728e03fc379
Mohammad Elbaba <mhmdbaba2001@gmail.com>
2022-03-23 11:48:03 +0200

    commit 3

--- app/src/main/java/com/example/dekanji/MainActivity.java
+++ app/src/main/java/com/example/dekanji/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Register (View view){
-        Intent reg = new Intent(this,register.class);
+        Intent reg = new Intent(this, register.class);
         startActivity(reg);
     }
 }
--- app/src/main/java/com/example/dekanji/register.java
+++ app/src/main/java/com/example/dekanji/register.java
@@ -5,13 +5,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class register extends AppCompatActivity {
 
+    private RadioGroup rd_grp;
+    private RadioButton rd_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        rd_grp = (RadioGroup) findViewById(R.id.radioGroup);
+        rd_btn = (RadioButton) findViewById(rd_grp.getCheckedRadioButtonId());
+
     }
 
+    public void Button (View view){
+        Button btn = (Button) view;
+
+        if (btn.getTag().toString().equalsIgnoreCase("Back")){
+            Intent reg = new Intent(this,MainActivity.class);
+            startActivity(reg);
+        }
+        else if (btn.getTag().toString().equalsIgnoreCase("submit")){
+            if (rd_btn.getText().toString().equalsIgnoreCase("buyer")){
+                //if buyer the app will direct the user to home page
+                //Intent reg = new Intent(this,Buyer.class);
+                //startActivity(reg);
+            }
+            else if (rd_btn.getText().toString().equalsIgnoreCase("Store owner")){
+                //if store owner the app will direct th user to fill more information about his store
+                Intent reg = new Intent(this,StoreOwner.class);
+                startActivity(reg);
+            }
+            else{
+                Toast.makeText(this,"Choose if Buyer or Store Owner!",Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
 }
