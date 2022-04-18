0a4d8afde6a70091a7202681c02806ec68ee3b97
Mohammad Elbaba <mhmdbaba2001@gmail.com>
2022-04-18 17:16:31 +0300

    commit 5

--- app/src/main/java/com/example/dekanji/MainActivity.java
+++ app/src/main/java/com/example/dekanji/MainActivity.java
@@ -6,8 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String input_username;
+    String input_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,17 +21,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Button (View view){
-        Button btn = (Button) view;
+        Button btn =(Button) view;
 
-        if (btn.getTag().toString().equalsIgnoreCase("register")){
-            Intent intent = new Intent(this, register.class);
-            startActivity(intent);
+        if (btn.getTag().toString().equalsIgnoreCase("login")){
+            //In case the user presses login
+            //check the user credentials in database and direct the user to home page
+            input_username = ((EditText) findViewById(R.id.input_username)).getText().toString();
+            input_password = ((EditText) findViewById(R.id.input_password)).getText().toString();
+
+            if (!input_password.isEmpty() && !input_username.isEmpty()) {
+                if (input_username.equals("admin") && input_password.equals("admin123")) {
+                    //Intent intent = new Intent(this, HomePage.class);
+                    //startActivity(intent);
+                }
+                else if (input_username != "admin" && input_password != "admin123"){
+                    //check if the credentials are found in the database
+                    //then direct to home screen
+                }
+                else {
+                    Toast.makeText(this,"Please enter correct username and password", Toast.LENGTH_LONG).show();
+                    return;
+                }
+            }
+            else {
+                Toast.makeText(this,"Please enter username and password", Toast.LENGTH_LONG).show();
+                return;
+            }
         }
-        else if (btn.getTag().toString().equalsIgnoreCase("login")){
-            Intent intent = new Intent(this, HomePage.class);
-            startActivity(intent);
+        else if (btn.getTag().toString().equalsIgnoreCase("register")){
+            //In case the user doesn't have an account
+            //Intent intent = new Intent(this, Register.class);
+            //startActivity(intent);
         }
-
-
     }
 }
