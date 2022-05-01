db2516da663b35549e37139f3deba76dac8d85fe
Mohammad Elbaba <mhmdbaba2001@gmail.com>
2022-05-01 13:55:44 +0300

    commit 20

--- app/src/main/java/com/example/dekanji/MyStore.java
+++ app/src/main/java/com/example/dekanji/MyStore.java
@@ -8,15 +8,30 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class mystore extends AppCompatActivity {
 
     String product_name;
     String product_price;
 
+    private FirebaseUser user;
+    private DatabaseReference referenceUsers;
+    private String userID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mystore);
+
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        referenceUsers = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
     }
 
     public void Button(View view){
@@ -31,6 +46,10 @@ public class mystore extends AppCompatActivity {
             //get inputs from user
             product_name = ((EditText) findViewById(R.id.input_product_name)).getText().toString();
             product_price = ((EditText) findViewById(R.id.input_price)).getText().toString();
+
+            if (!product_name.isEmpty() && !product_price.isEmpty()){
+
+            }
 
 
 
---
+++ app/src/main/java/com/example/dekanji/Products.java
@@ -0,0 +1,37 @@
+package com.example.dekanji;
+
+public class Products {
+    private String userID;
+    private String productName;
+    private String price;
+
+    public Products(String userID, String productName, String price) {
+        this.userID = userID;
+        this.productName = productName;
+        this.price = price;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID (String userID){
+        this.userID = userID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
