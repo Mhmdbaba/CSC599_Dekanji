348155601132d3dfd187008a5c87a0ca6028ce35
Mohammad Elbaba <mhmdbaba2001@gmail.com>
2022-05-07 14:07:23 +0300

    commit 40

--- app/src/main/java/com/example/dekanji/MyAdapter.java
+++ app/src/main/java/com/example/dekanji/MyAdapter.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -34,6 +36,24 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         Products products = list.get(position);
         holder.product_name.setText(products.getProductName());
         holder.price.setText(products.getPrice());
+        holder.txt_options.setOnClickListener(v->
+        {
+            PopupMenu popupMenu = new PopupMenu(context, holder.txt_options);
+            popupMenu.inflate(R.menu.options_menu);
+            popupMenu.setOnMenuItemClickListener(item->
+            {
+                if (item.getItemId() == R.id.menu_edit){
+                    Toast.makeText(context, "edit", Toast.LENGTH_SHORT).show();
+
+                }
+                if (item.getItemId() == R.id.menu_remove){
+                    Toast.makeText(context, "remove", Toast.LENGTH_SHORT).show();
+
+                }
+                return false;
+            });
+            popupMenu.show();
+        });
     }
 
     @Override
@@ -43,13 +63,14 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     public static class MyViewHolder extends RecyclerView.ViewHolder{
 
-        TextView product_name, price;
+        TextView product_name, price, txt_options;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
             product_name = itemView.findViewById(R.id.tv_product_name);
             price = itemView.findViewById(R.id.tv_product_price);
+            txt_options = itemView.findViewById(R.id.txt_options);
         }
     }
 }
